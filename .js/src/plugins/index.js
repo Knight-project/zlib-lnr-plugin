a0205819e07d3b04df341f484136c4ba2d1d632c
Knight-project <vallrayn26@gmail.com>
2025-06-04 11:58:11 +0600

    chore: Publish Plugins

---
+++ .js/src/plugins/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var zlib_1 = __importDefault(require("./english/zlib"));
+var PLUGINS = [zlib_1.default];
+exports.default = PLUGINS;
